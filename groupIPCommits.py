0040c1499b1098c4ae9f1b2450660c8fe3170769
faizan khan <faizan10114@gmail.com>
2019-02-22 18:18:31 -0500

    first commit

---
+++ groupIPCommits.py
@@ -0,0 +1,41 @@
+
+from collections import defaultdict
+from collections import namedtuple
+
+f= open("log.txt")
+
+lines = [line for line in f.readlines()]
+
+# CommitIssue = namedtuple('CommitIssue', ('commit', 'issue'))
+# commitIssueArray=[]
+issueHash = defaultdict(list)
+
+
+
+for line in lines:
+	try:
+		commit, msg = line.split("&=&")
+	except ValueError:
+		print(line)
+
+	msgArray = msg.split(" ")
+
+	if msgArray[0]=="Revert":
+		msgArray.insert(0, msgArray[1])
+
+	issueNumber = msgArray[0]
+
+
+	issueHash[issueNumber].append(commit)
+	
+	# commitIssueArray.append(CommitIssue(commit, msgArray[0].split("-")[1]))
+
+
+
+# for val in commitIssueArray:
+# 	print(val.commit, val.issue)
+
+for key,val in issueHash.items():
+	print(key,val)
+
+print(len(lines))
