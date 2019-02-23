2ace474ac376e55309ee07dff16decef7e8254fc
faizan khan <faizan10114@gmail.com>
2019-02-23 07:16:22 -0500

    refined the graph structure, added the calling, and called edges

--- CommitGraph/getGraphFromPatch.py
+++ CommitGraph/getGraphFromPatch.py
@@ -1,12 +1,14 @@
 import json
 from enum import Enum
 
+from itertools import combinations
+
 
 class EdgeType(Enum):
-	Calling = 0.21
-	Called = 0.21
-	Def = 0.42
-	Equal = 1
+	CALLING = 0.21
+	CALLED = 0.21
+	DEF = 0.42
+	EQUAL = 1
 
 class Node:
 	def __init__(self, name):
@@ -34,6 +36,9 @@ class Graph:
 	def addNode(self, node):
 		self.nodes.add(node)
 
+	def getNodeNames(self):
+		return [node.name for node in graph.nodes]
+
 
 if __name__=='__main__':
 	A='''{"JsonMapper": {"writeMapAsString": "writeObjectAsString", "writeObjectAsString":"", "JsonMapper":""}}'''
@@ -42,28 +47,46 @@ if __name__=='__main__':
 	print(hashMap)
 	className = list(hashMap.keys())[0]
 
-	graph = Graph()
-	for key in hashMap[className]:
-		node = Node(key)
 
-		if node not in graph.nodes:
+	graph = Graph()
+	for k,v in hashMap[className].items():
+		node = Node(k)
+
+		if v !="":
+			endingNode = Node(v)
+			e = Edge(endingNode, EdgeType.CALLING)
+			node.outGoingEdges.add(e)
+			e2 = Edge(node, EdgeType.CALLED)
+			endingNode.outGoingEdges.add(e2)
+			if endingNode.name not in graph.getNodeNames():
+				graph.addNode(endingNode)
+
+		if node.name not in graph.getNodeNames():
 			graph.addNode(node)
+
 
 	#create an between each node
 
-	nodes = list(graph.nodes)
+	for n1, n2 in list(combinations(graph.nodes,2)):
+		e = Edge(n1, EdgeType.DEF)
+		n2.outGoingEdges.add(e)
+		e = Edge(n2, EdgeType.DEF)
+		n1.outGoingEdges.add(e)
 
-	e = Edge(nodes[0], EdgeType.Def)
 
-	for node in nodes[1:]:
-		node.outGoingEdges.add(e)
-		e = Edge(node, EdgeType.Def)
+	# nodes = list(graph.nodes)
 
-	e = Edge(nodes[-1], EdgeType.Def)
-
-	for node in reversed(nodes[:-1]):
-		node.outGoingEdges.add(e)
-		e = Edge(node, EdgeType.Def)
+	# e = Edge(nodes[0], EdgeType.Def)
+	#
+	# for node in nodes[1:]:
+	# 	node.outGoingEdges.add(e)
+	# 	e = Edge(node, EdgeType.Def)
+	#
+	# e = Edge(nodes[-1], EdgeType.Def)
+	#
+	# for node in reversed(nodes[:-1]):
+	# 	node.outGoingEdges.add(e)
+	# 	e = Edge(node, EdgeType.Def)
 
 
 
