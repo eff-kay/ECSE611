7bd0d9a52db8867fc3d7945511e88f0f811613d6
faizan khan <faizan10114@gmail.com>
2019-03-02 19:43:53 -0500

    tested the djikstra implementation with my Graph

--- CommitGraph/getGraphFromPatch.py
+++ CommitGraph/getGraphFromPatch.py
@@ -3,6 +3,9 @@ from enum import Enum
 
 from itertools import combinations
 
+from dijkstar import Graph as dGraph, find_path
+
+
 
 class EdgeType(Enum):
 	CALLING = 0.21
@@ -41,10 +44,10 @@ class Graph:
 
 
 if __name__=='__main__':
-	A='''{"JsonMapper": {"writeMapAsString": "writeObjectAsString", "writeObjectAsString":"", "JsonMapper":""}}'''
+	A='''{"JsonMapper": {"writeMapAsString": "writeObjectAsString", "writeObjectAsString":"toJSON", "JsonMapper":""}}'''
 	hashMap = json.loads(A)
 
-	print(hashMap)
+	# print(hashMap)
 	className = list(hashMap.keys())[0]
 
 
@@ -65,13 +68,50 @@ if __name__=='__main__':
 			graph.addNode(node)
 
 
-	#create an between each node
+	#create a def relation between each node
 
 	for n1, n2 in list(combinations(graph.nodes,2)):
 		e = Edge(n1, EdgeType.DEF)
 		n2.outGoingEdges.add(e)
 		e = Edge(n2, EdgeType.DEF)
 		n1.outGoingEdges.add(e)
+
+
+	g = dGraph()
+	# for node in graph.nodes:
+	# 	for edge in node.outGoingEdges:
+	# 		g.add_edge(node.name, edge.endingNode.name, {'cost': edge.edgeType.value})
+
+
+
+	g.add_edge('writeMapAsString', 'writeObjectAsString', {'cost': 0.21})
+	g.add_edge('writeObjectAsString', 'writeMapAsString', {'cost': 0.21})
+	g.add_edge('writeObjectAsString', 'toJSON', {'cost': 0.21})
+	g.add_edge('toJSON', 'writeObjectAsString', {'cost': 0.21})
+	g.add_edge('JsonMapper', 'writeMapAsString', {'cost': 0.42})
+	g.add_edge('writeMapAsString', 'JsonMapper', {'cost': 0.42})
+	g.add_edge('JsonMapper', 'writeObjectAsString', {'cost': 0.42})
+	g.add_edge('writeObjectAsString', 'writeMapAsString', {'cost': 0.42})
+	g.add_edge('writeMapAsString', 'writeObjectAsString', {'cost': 0.42})
+
+
+	# g.add_edge('B', 'D', {'cost': 2})
+	# g.add_edge('E', 'D', {'cost': 1})
+	# g.add_edge('A', 'C', {'cost': 2})
+	# g.add_edge('C', 'E', {'cost': 5})
+	# g.add_edge('B', 'C', {'cost': 3})
+	# g.add_edge('C', 'B', {'cost': 1})
+
+	cost_func = lambda u, v, e, prev_e: e['cost']
+	#
+	print(find_path(g, 'writeMapAsString', 'toJSON', cost_func=cost_func))
+	# print(find_path(g, 'A', 'C', cost_func=cost_func))
+	# print(find_path(g, 'A', 'D', cost_func=cost_func))
+	# print(find_path(g, 'A', 'E', cost_func=cost_func))
+
+
+
+
 
 
 	# nodes = list(graph.nodes)
