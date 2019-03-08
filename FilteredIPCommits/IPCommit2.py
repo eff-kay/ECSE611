96b85a5fdb4ece9c8c9e552f4eafaf258b322a33
shivanibali <sbali@blockport.io>
2019-03-08 15:05:04 -0500

    Code filtered
    picking up parent and relative commits

---
+++ FilteredIPCommits/IPCommit2.py
@@ -0,0 +1,45 @@
+import pprint
+from collections import defaultdict
+import re
+from collections import namedtuple
+
+def fileParse(filepath):
+
+    lines = open(filepath, "r")
+    mapID = {}
+    ipCommits = {}
+    unusedArray = []
+    lastDefectNum = ''
+    for line in lines:
+        commit, issueID = line.strip().split("&=&")
+        # print(commit, issueID)
+        defectInfo = issueID.strip().split(" ")
+        # print(defectInfo)
+        defectNum = defectInfo[0]
+
+        pp = pprint.PrettyPrinter(indent=4)
+
+        # pp.pprint(initial)
+        if not lastDefectNum == '':
+            mapID[lastDefectNum][-1] = (mapID[lastDefectNum][-1][0], commit)
+
+        # if not defectNum.isupper or not re.match(r'[H|E]',defectNum):
+        # if not defectNum.isupper or not re.match(r'[H]', defectNum):
+        #     unusedArray.append(defectNum)
+
+        if defectNum in mapID:
+            mapID[defectNum].append((commit, ''))
+            lastDefectNum = defectNum
+
+        else:
+            mapID[defectNum] = [(commit, '')]
+            lastDefectNum = defectNum
+
+    for key, val in mapID.items():
+       if key.isupper and len(val) > 1 and re.match(r'[H]', key):
+        ipCommits[key] = val
+
+    pp.pprint(ipCommits)
+
+
+fileParse("log.txt")
