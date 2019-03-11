8bce2bb97ceffad48184076fa21dba347c610036
karanshundal7 <karan.hundal@mail.mcgill.ca>
2019-03-11 01:55:12 -0400

    Trying to count binaries as usual

---
+++ CommitGraph/check_binary.py
@@ -0,0 +1,40 @@
+from patch_reader import PatchReader
+
+import ast
+
+
+def write_to_file(bin):
+    with open("binary_count.txt","a") as myfile:
+        myfile.write(bin+"\n")
+
+if __name__ == '__main__':
+    with open('Issue2Commits.txt', 'r') as f:
+        s = f.read()
+        fileData = ast.literal_eval(s)
+    score = 0
+    methods1 = methods2 = []
+    patchName = ""
+
+    for k, v in fileData.items():
+        print("For issue Number: ", k)
+        temp_list = ""
+        project = 'hbase'
+        commitId1 = v[0][0]
+        commitId2 = v[0][1]
+
+        project = 'hbase'
+        commitId3 = v[1][0]
+        commitId4 = v[1][1]
+        patchName = ""
+        try:
+            patchName = commitId1+commitId2+".patch"
+            methods1 = PatchReader(project, commitId1, commitId2).methods()
+            patchName = commitId3 + commitId4 + ".patch"
+            methods2 = PatchReader(project, commitId3, commitId4).methods()
+            if len(methods1)==0 or len(methods2):
+                temp_list = k+","+commitId1+" "+commitId2+" "+commitId3+" "+commitId4+","+str(score)
+                write_to_file(temp_list)
+        except Exception:
+            pass
+
+    print(temp_list)
